cc1e94fb998e5dccce6e1b432236f38387e9cbad
Thecaracter <92218072+Thecaracter@users.noreply.github.com>
2024-04-02 04:00:59 +0700

    perbaikan minor

--- resources/views/layouts/app-auth.blade.php
+++ resources/views/layouts/app-auth.blade.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="{{ asset('admin/assets/css/components.css') }}">
     <!-- Custom style CSS -->
     <link rel="stylesheet" href="{{ asset('admin/assets/css/custom.css') }}">
-    <link rel='shortcut icon' type='image/x-icon' href='{{ asset('admin/assets/img/favicon.ico') }}' />
+    <link rel='shortcut icon' type='image/x-icon' href='foto/logo.png' />
 </head>
 
 <body>
